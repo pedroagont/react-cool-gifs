3fa42e1c6a47af939eff2b922a3a4f74591cacf8
Pedro A. González <pgonzalez@post.com>
2021-08-11 04:32:54 -0500

    Render main App component + styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { Gif } from "@giphy/react-components";
+import SearchExperience from './components/SearchExperience';
 import './App.css';
 
 function App() {
+  const [modalGif, setModalGif] = useState();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <h4 className="title">Search GIFs</h4>
+      <SearchExperience
+          onGifClick={(gif, e) => {
+          console.log("gif", gif);
+          e.preventDefault();
+          setModalGif(gif);
+        }}
+      />
+      {modalGif && (
+        <div
+          style={{
+            position: "fixed",
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            background: "rgba(0, 0, 0, .8)"
+          }}
+          onClick={(e) => {
+            e.preventDefault();
+            setModalGif(undefined);
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Gif gif={modalGif} width={200} />
+        </div>
+      )}
+    </>
+  )
 }
 
 export default App;
